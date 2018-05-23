e1d9f3d0e98459ee3006089f44aeb75f66d4dbbd
saviravevo <36556553+saviravevo@users.noreply.github.com>
2018-05-23 14:33:03 +0700

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -2,6 +2,7 @@ from pyspark.ml.linalg import Vectors
 from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
 from pyspark.ml.regression import LinearRegression
+from pyspark.ml.regression import LinearRegressionModel
 from pyspark.ml.tuning import (CrossValidator, ParamGridBuilder)
 from pyspark.ml.evaluation import RegressionEvaluator
 
@@ -32,8 +33,17 @@ def linear_reg(train_df, conf):
             lrModel = lr.fit(train_df)
             
         return lrModel
- 
-    
+
+#Save Model
+def saveModel(model, path):
+    model.save(path)
+    return
+
+#Load Model
+def loadModel(path):
+    model = LinearRegressionModel.load(path)
+    return model
+
 #Making Prediction using test data
 def predict(test_df, model):
         """ input   : df [spark.dataframe], linear_regression model [model]
@@ -42,7 +52,6 @@ def predict(test_df, model):
         val = model.transform(test_df)
         prediction = val.select("label","prediction")
         return prediction
-    
     
 #Showing R-square using test data
 def r_square(df, col_prediction, col_label):
@@ -55,7 +64,18 @@ def r_square(df, col_prediction, col_label):
         r2 = [(Vectors.dense(r2),)]
         r2_df = spark.createDataFrame(r2, ["R-square"])
         return r2_df
-  
+
+#Showing RMSE using test data
+def rmse(df, col_prediction, col_label):
+        """ input : df [spark.dataframe]
+            output : RMS on test data [float]
+        """    
+        lr_evaluator = RegressionEvaluator(predictionCol=col_prediction, 
+                 labelCol=col_label, metricName="rmse")
+        rmse =  lr_evaluator.evaluate(df)
+        rmse = [(Vectors.dense(rmse),)]
+        rmse_df = spark.createDataFrame(rmse, ["RMS"])
+        return rmse_df 
     
 #Showing selected row
 def row_slicing(df, n):
@@ -66,23 +86,28 @@ def row_slicing(df, n):
 
 
 
-
 #load input data
 linear_df = spark.read.format("libsvm")\
         .load("C:/Users/Lenovo/spark-master/data/mllib/sample_linear_regression_data.txt")
 
 #Splitting data into training and test
-training, test = linear_df.randomSplit([0.6, 0.4], seed=11)
-training.cache()        
+training, test = linear_df.randomSplit([0.7, 0.3])       
+training.cache()
     
 #Config Dictionary
 config      = {
-               "params" : {"maxIter" : 50, "regParam" : 0.2, "elasticNetParam": 0.7},
-               "crossval" : {"crossval" : False, "N" : 5, "metricName" : "r2"}
-               }
-    
+               "params" : {"maxIter" : 10, "regParam" : 0.3, "elasticNetParam": 0.8},
+               "crossval" : {"crossval" : False, "N" : 10, "metricName" : "r2"}
+               }   
 
-
+#getting model
 model = linear_reg(training, config)
-prediction = predict(test, model)
-rsq = r_square(prediction, "prediction", "label")  
+
+#getting prediction
+testing = predict(test, model)
+
+#getting R-square
+rsq = r_square(testing, "prediction", "label")  
+
+#getting RMS
+rms= rmse(testing, "prediction", "label")
