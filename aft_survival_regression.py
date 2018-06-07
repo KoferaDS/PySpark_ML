2c725914b976bd5a36e4804e86f0a701713fd9a7
Insanaprilian <39475797+Insanaprilian@users.noreply.github.com>
2018-06-07 11:51:10 +0700

    Add files via upload

---
+++ aft_survival_regression.py
@@ -0,0 +1,233 @@
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.tuning import CrossValidator,ParamGridBuilder, TrainValidationSplit
+from pyspark.ml.tuning import CrossValidationModel, TrainValidationSplitModel
+from pyspark.ml.regression import AFTSurvivalRegression, AFTSurvivalRegressionModel
+from pyspark.context import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.ml.linalg import Vectors
+
+sc = SparkContext.getOrCreate()
+spark = SparkSession(sc)  
+ 
+# Set parameter and its value for AFT Survival Regression
+afts_params = {
+                    "predictionCol" : "prediction",
+                    "labelCol" : "label",
+                    "featuresCol" : "features",
+                    "censorCol" : "censor",
+                    "quantilesCol" : None,
+                    "fitIntercept" : True,
+                    "maxIter" : 100,
+                    "tol" : 1E-6,
+                    "quantileProbabilities" : [0.01, 0.05, 0.1, 0.25, 
+                                               0.5, 0.75, 0.9, 0.95, 0.99],
+                    "aggregationDepth" : 2
+                  }
+            
+# Set params tuning
+# method : "crossval" , "trainvalsplit"
+# methodParams is set as : - fold for "crossval" (value : f>0) 
+#                          - trainratio for "trainvalsplit" (value: 0<tr<1)
+tune_params = {
+                "method" : "trainvalsplit",
+                "methodParams" : 0.6
+              }
+
+# Set configuration whether use tuning/non-tuning
+conf   =  {
+                "params" : afts_params,
+                "tuning" : None
+            }
+
+conf2  =   {
+                "params" : afts_params,
+                "tuning" : tune_params
+           }
+
+def aftsurvivalRegression(df, conf):
+  """ AFT Survival Regression training
+        Input  : - Dataframe of training (df)
+                 - tuning and hiperparameter configuration (conf)
+        output : - AFT survival regression model (model)
+  """
+  feature_col = conf["params"].get("featuresCol", "features")
+  label_col = conf["params"].get("labelCol", "label")
+  pred_col = conf["params"].get("predictionCol", "prediction")
+  cens_col = conf["params"].get("censorCol", "censor")
+  fit_intercept = conf["params"].get("fitIntercept",True)
+  max_iter = conf["params"].get("maxIter", 100)
+  tol = conf["params"].get("tol", )
+  quant_p = conf["params"].get("quantileProbabilities", [0.01, 0.05, 0.1, 0.25, 
+                                                        0.5, 0.75, 0.9, 0.95, 0.99])
+  quant_col = conf["params"].get("quantilesCol", None)
+  agg_depth = conf["params"].get("aggregationDepth", 2)
+      
+  afts = AFTSurvivalRegression(featuresCol=feature_col,labelCol=label_col,
+                          predictionCol=pred_col, censorCol=cens_col,
+                          maxIter=max_iter, fitIntercept=fit_intercept,
+                          tol=tol, aggregationDepth=agg_depth)
+
+  if conf["tuning"]:
+    if conf["tuning"].get("method").lower() == "crossval":
+      folds = conf["tuning"].get("methodParam", 2)
+      # Set the hiperparameter that we want to grid, incase: maxIter and aggregationDepth
+      pg = ParamGridBuilder()\
+                .addGrid(afts.maxIter,[10, 50, 100])\
+                .addGrid(afts.aggregationDepth[3,4,5])\
+                .build()
+      evaluator = RegressionEvaluator()
+      cv = CrossValidator(estimator=afts, estimatorParamMaps=pg,
+                          evaluator=evaluator, numFolds=folds)
+      model = cv.fit(df)
+      
+    elif conf["tuning"].get("method").lower() == "trainvalsplit":
+      tr = conf["tuning"].get("methodParam", 0.8)
+      # Set the hiperparameter that we want to grid, incase: maxIter and aggregationDepth
+      pg = ParamGridBuilder()\
+                .addGrid(afts.maxIter,[10, 50, 100])\
+                .addGrid(afts.aggregationDepth[3,4,5])\
+                .build()
+      evaluator = RegressionEvaluator()
+      tvs = TrainValidationSplit(estimator=afts, estimatorParamMaps=pg,
+                                 evaluator=evaluator, trainRatio=tr)
+      model = tvs.fit(df)
+  elif conf["tuning"] ==  None:
+    model = afts.fit(df)
+  return model
+
+def saveModel(model,path):
+    '''Save model into corresponding path.
+       Input  : - Model
+                - Path
+       Output : - Saved model
+    '''
+    model.save(path)
+    return
+
+def loadModel(path):
+    '''Loading model from path.
+       Input  : - Path
+       Output : - Loaded model
+    '''
+  # Load model if use crossvalidation tuning
+    if conf["tuning"].get("method") == "crossval" :
+        loaded_model = CrossValidationModel.load(path)   
+  # Load model if use trainvalidationsplit tuning
+    elif conf["tuning"].get("method") == "trainval":
+        loaded_model = TrainValidationSplitModel.load(path)
+  # Load model if non-tuning
+    elif conf["tuning"].get("method") == None:
+        loaded_model = AFTSurvivalRegressionModel.load(path)
+    return loaded_model
+
+def predict(df, model):
+    """ Prediction value by the trained model
+        Input  : -Dataframe test(df)
+                 -Trained model (model)
+        Output : -Dataframe with prediction column (transformed)
+    """      
+    transformed = model.transform(df).select("label","prediction","censor")
+    return transformed
+    
+def prediction(df,model):
+    """ show dataframe of prediction in kernel
+         Input  : -Dataframe test(df)
+                  -Trained model (model)
+        Output :  -Dataframe display with prediction column (transformed)
+    """
+    model.transform(df).show()
+
+def summary_R2(df):
+    """ Root square value from the model
+        input  : -Dataframe prediction (df)
+        output : -Dataframe of R^2 and Rms (df)
+    """    
+    evaluator = RegressionEvaluator(metricName="r2")
+    R2      = evaluator.evaluate(df)
+    v_R2    = [(Vectors.dense(R2),)]
+    df_R2   = spark.createDataFrame(v_R2, ["R^2"])  
+    return df_R2
+
+def summary_Rmse(df):
+    """ Root mean square value from the model
+        input  : -Dataframe prediction (df)
+        output : -Dataframe of R^2 and Rms (df)
+    """    
+    evaluator = RegressionEvaluator(metricName="rmse")
+    Rmse     = evaluator.evaluate(df)
+    v_Rmse   = [(Vectors.dense(Rmse),)]
+    df_Rmse  = spark.createDataFrame(v_Rmse, ["Rmse"])  
+    return df_Rmse
+
+def summary_coefficient(model):
+    #Only if the model is AFTSurvivalRegressionModel
+    """
+        input : - trained model
+        output : - model coefficient value
+    """
+    coe = model.coefficients
+    return coe
+
+def summary_intercept(model):
+    #Only if the model is AFTSurvivalRegressionModel
+    """
+        input : - trained model
+        output : - model intercept value
+    """
+    intercept = model.intercept
+    return intercept
+
+def summary_quantile(model):
+    #Only if the model is AFTSurvivalRegressionModel
+    """
+        input : - trained model
+        output : - predicted quantiles value
+    """
+    pq = model.predictQuantiles
+    return pq
+    
+def copyModel(model):
+    copy_model = model.copy(extra=None)
+    return copy_model
+
+# ------------------------------Test and examples--------------------------------
+
+#     Loads dataframe
+  
+df_afts = spark.read.format("libsvm")\
+            .load("D:\Kofera\spark-master\data\mllib\sample_libsvm_data.txt")    
+              
+#     Splitting dataframe into dataframe training and test 
+#     incase: 0.6 (70%) datainput used as df training and 0.4 (30%) used as df test
+df_training, df_test = df_afts.randomSplit[0.6, 0.4]
+df_training.cache()
+   
+#     Applied methods to Data
+# AFT Survival Model
+trained_model = aftsurvivalRegression(df_training,conf2)
+
+#Save model
+save = saveModel(trained_model, "path")
+
+#Load model
+loaded_model = loadModel("path")
+
+#Prediction
+testing = predict(df_test,trained_model)
+testing.show()
+
+#Root square
+r2      = summary_R2(testing)  
+r2.show() 
+#Root mean square
+rmse    = summary_Rmse(testing)  
+rmse.show()
+#Model Coefficient
+coeff = summary_coefficient(trained_model)
+coeff.show()
+#Model Intercept
+interc = summary_intercept(trained_model)
+interc.show()
+#QuantilePrediction
+qp = summary_quantile(trained_model)
+qp.show()
