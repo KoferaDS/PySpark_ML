20fca22078baa57baa00b701d8ddec3187933fc9
Insanaprilian <39475797+Insanaprilian@users.noreply.github.com>
2018-07-18 12:20:23 +0700

    Update regression_params.py

--- regression_params.py
+++ regression_params.py
@@ -46,7 +46,7 @@
              },	
              {
                "name" : "quantileProbabilities"
-               "type" : list
+               "type" : "list"
 	       "default value": [0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99]
              },
              {
